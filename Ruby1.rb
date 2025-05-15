61e08bedcdef54e9429ddcfed8ba0ad11f2fc79a
Max <maxigiaco94@hotmail.com>
2025-05-15 18:49:22 -0300

    Start 1st ruby file. Prepare for installations

---
+++ Ruby1.rb
@@ -0,0 +1,6 @@
+#This is the beginning of our course on ruby. The TOB starts with teachin Ruby, then going on with Rails and eventually ramping on the difficulty of the exercises. When working on ruby, indentation (tab) should be set to 2 spaces. With Ctrl+Shift+P we can search for "language specific settings" and tweak VSCode for this.
+#Some initial conventions. When defining methods or variables, we should use snake_case formatting. This way, what used to be aVeryCoolVariable should be a_very_cool_variable. Related to this, whenever we declare a constant we should use all uppercase so that we get CONTSANT_VAR. 
+#HOWEVER, whenever we declare a class we should use PascalCase instead, ending up with stuff like MyFirstClass.
+#Sort of unrelated, but the API acronym may also refer to docummentation in certain circles.  
+
+#Before anything we have to install ruby.
